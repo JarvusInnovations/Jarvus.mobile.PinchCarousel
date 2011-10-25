e2d5fe07021ede465350289aa35a02eab022ece4
Chris Alfano <themightychris@gmail.com>
2011-10-25 18:54:23 -0400

    Extracted pinch-to-zoom functionality from PinchCarousel to new class called ImageViewer so you can use the pinch functionality without the carousel

---
+++ ImageViewer.js
@@ -0,0 +1,303 @@
+Ext.ns('Jarvus.mobile');
+
+Jarvus.mobile.ImageViewer = Ext.extend(Ext.Component, {
+
+	doubleTapScale: 1
+	,maxScale: 1
+	,loadingMask: true
+	,previewSrc: false
+	,imageSrc: false
+
+	,cls: 'imageBox'
+	,scroll: 'both'
+	,html: [
+		'<figure><img></figure>'
+	]
+
+	,initComponent: function() {
+		Jarvus.mobile.ImageViewer.superclass.initComponent.apply(this, arguments);
+	
+		this.on('activate', function() {
+		
+			//	disable scroller
+			this.scroller.disable();
+
+			// mask image viewer
+			if(this.loadingMask)
+				this.el.mask(Ext.LoadingSpinner);
+
+			// retrieve DOM els
+			this.figEl = this.el.down('figure');
+			this.imgEl = this.figEl.down('img');
+
+			// apply required styles
+			this.figEl.setStyle({
+				overflow: 'hidden'
+				,display: 'block'
+				,margin: 0
+			});
+
+			this.imgEl.setStyle({
+				'-webkit-user-drag': 'none'
+				,'-webkit-transform-origin': '0 0'
+				,'visibility': 'hidden'
+			});
+
+			// show preview
+			if(this.previewSrc)
+			{
+				this.el.setStyle({
+					backgroundImage: 'url('+this.previewSrc+')'
+					,backgroundPosition: 'center center'
+					,backgroundRepeat: 'no-repeat'
+					,webkitBackgroundSize: 'contain'
+				});
+			}
+
+			// attach event listeners
+			this.mon(this.imgEl, {
+				scope: this
+				,load: this.onImageLoad
+				,doubletap: this.onDoubleTap
+				,pinchstart: this.onImagePinchStart
+				,pinch: this.onImagePinch
+				,pinchend: this.onImagePinchEnd
+			});
+
+			// load image
+			if(this.imageSrc)
+				this.loadImage(this.imageSrc);
+				
+		}, this, {delay: 10, single: true});
+
+	}
+	
+	,loadImage: function(src) {	
+		if(this.imgEl)
+			this.imgEl.dom.src = src;
+		else
+			this.imageSrc = src;
+			
+	}
+
+	
+	,onImageLoad: function() {
+		// get viewport size
+		this.viewportWidth = this.viewportWidth || this.getWidth() || this.ownerCt.body.getWidth();
+		this.viewportHeight = this.viewportHeight || this.getHeight() || this.ownerCt.body.getHeight();
+			
+		// grab image size
+		this.imgWidth = this.imgEl.dom.width
+		this.imgHeight = this.imgEl.dom.height;
+				
+		// calculate and apply initial scale to fit image to screen
+		if(this.imgWidth > this.viewportWidth || this.imgHeight > this.viewportHeight)
+			this.scale = this.baseScale = Math.min(this.viewportWidth/this.imgWidth, this.viewportHeight/this.imgHeight);
+		else
+			this.scale = this.baseScale = 1;
+		
+		// set initial translation to center
+		this.translateX = this.translateBaseX = (this.viewportWidth - this.baseScale * this.imgWidth) / 2;
+		this.translateY = this.translateBaseY = (this.viewportHeight - this.baseScale * this.imgHeight) / 2;
+		
+		// apply initial scale and translation
+		this.applyTransform();
+		
+		// initialize scroller configuration
+		this.adjustScroller();
+
+		// show image and remove mask
+		this.imgEl.setStyle({ visibility: 'visible' });
+
+		// remove preview
+		if(this.previewSrc)
+		{
+			this.el.setStyle({
+				backgroundImage: 'none'
+			});
+		}
+
+		if(this.loadingMask)
+			this.el.unmask();
+
+		this.fireEvent('imageLoaded', this);
+	}
+	
+	,onImagePinchStart: function(ev) {
+		// disable scrolling during pinch
+		this.scroller.stopMomentumAnimation();
+		this.scroller.disable();
+		
+		// store beginning scale
+		this.startScale = this.scale;
+		
+		// calculate touch midpoint relative to image viewport
+		this.originViewportX = (ev.touches[0].clientX + ev.touches[1].clientX) / 2 - this.el.getX();
+		this.originViewportY = (ev.touches[0].clientY + ev.touches[1].clientY) / 2 - this.el.getY();
+		
+		// translate viewport origin to position on scaled image
+		this.originScaledImgX = this.originViewportX - this.scroller.offset.x - this.translateX;
+		this.originScaledImgY = this.originViewportY - this.scroller.offset.y - this.translateY;
+		
+		// unscale to find origin on full size image
+		this.originFullImgX = this.originScaledImgX / this.scale;
+		this.originFullImgY = this.originScaledImgY / this.scale;
+		
+		// calculate translation needed to counteract new origin and keep image in same position on screen
+		this.translateX += (-1 * ((this.imgWidth*(1-this.scale)) * (this.originFullImgX/this.imgWidth)));
+		this.translateY += (-1 * ((this.imgHeight*(1-this.scale)) * (this.originFullImgY/this.imgHeight)))
+	
+		// apply new origin
+		this.setOrigin(this.originFullImgX, this.originFullImgY);
+	
+		// apply translate and scale CSS
+		this.applyTransform();
+	}
+	
+	,onImagePinch: function(ev) {
+		// prevent scaling to smaller than screen size
+		this.scale = Ext.util.Numbers.constrain(ev.scale * this.startScale, this.baseScale, this.maxScale);
+		this.applyTransform();
+	}
+	
+	,onImagePinchEnd: function(ev) {
+	
+		// set new translation
+		if(this.scale == this.baseScale)
+		{
+			// move to center
+			this.setTranslation(this.translateBaseX, this.translateBaseY);
+		}
+		else
+		{
+			// calculate rescaled origin
+			this.originReScaledImgX = this.originScaledImgX * (this.scale / this.startScale);
+			this.originReScaledImgY = this.originScaledImgY * (this.scale / this.startScale);
+			
+			// maintain zoom position
+			this.setTranslation(this.originViewportX - this.originReScaledImgX, this.originViewportY - this.originReScaledImgY);			
+		}
+		// reset origin and update transform with new translation
+		this.setOrigin(0, 0);
+		this.applyTransform();
+
+		// adjust scroll container
+		this.adjustScroller();
+	}
+
+	
+	,onDoubleTap: function(ev, t) {
+		
+		if(!this.doubleTapScale)
+			return false;
+		
+		// set scale and translation
+		if(this.scale >= .9)
+		{
+			// zoom out to base view
+			this.scale = this.baseScale;
+			this.setTranslation(this.translateBaseX, this.translateBaseY);
+		}
+		else
+		{
+			// zoom in toward tap position
+			var oldScale = this.scale
+				,newScale = 1
+				,originViewportX = ev ? (ev.pageX - this.el.getX()) : 0
+				,originViewportY = ev ? (ev.pageY - this.el.getY()) : 0
+				,originScaledImgX = originViewportX - this.scroller.offset.x - this.translateX
+				,originScaledImgY = originViewportY - this.scroller.offset.y - this.translateY
+				,originReScaledImgX = originScaledImgX * (newScale / oldScale)
+				,originReScaledImgY = originScaledImgY * (newScale / oldScale);
+				
+			this.scale = newScale;
+			this.setTranslation(originViewportX - originReScaledImgX, originViewportY - originReScaledImgY);
+		}
+			
+		// reset origin and update transform with new translation
+		this.applyTransform();
+
+		// adjust scroll container
+		this.adjustScroller();
+		
+		// force repaint to solve occasional iOS rendering delay
+		Ext.repaint();
+	}
+	
+	,setOrigin: function(x, y) {
+		this.imgEl.dom.style.webkitTransformOrigin = x+'px '+y+'px';
+	}
+	
+	,setTranslation:  function(translateX, translateY) {
+		this.translateX = translateX;
+		this.translateY = translateY;
+			
+		// transfer negative translations to scroll offset
+		this.scrollX = this.scrollY = 0;
+		
+		if(this.translateX < 0)
+		{
+			this.scrollX = this.translateX;
+			this.translateX = 0;
+		}
+		if(this.translateY < 0)
+		{
+			this.scrollY = this.translateY;
+			this.translateY = 0;
+		}
+	}
+		
+
+	,applyTransform: function() {
+	
+		var fixedX = Ext.util.Numbers.toFixed(this.translateX,5)
+			,fixedY = Ext.util.Numbers.toFixed(this.translateY,5)
+			,fixedScale = Ext.util.Numbers.toFixed(this.scale, 8);
+	
+		if(Ext.is.Android)
+		{
+			this.imgEl.dom.style.webkitTransform = 
+				//'translate('+fixedX+'px, '+fixedY+'px)'
+				//+' scale('+fixedScale+','+fixedScale+')';
+				'matrix('+fixedScale+',0,0,'+fixedScale+','+fixedX+','+fixedY+')'
+		}
+		else
+		{
+			this.imgEl.dom.style.webkitTransform =
+				'translate3d('+fixedX+'px, '+fixedY+'px, 0)'
+				+' scale3d('+fixedScale+','+fixedScale+',1)';
+		}
+		
+	}
+
+
+	,adjustScroller: function() {
+	
+		// disable scrolling if zoomed out completely, else enable it
+		if(this.scale == this.baseScale)
+			this.scroller.disable();
+		else
+			this.scroller.enable();
+		
+		// size container to final image size
+		var boundWidth = Math.max(this.imgWidth * this.scale, this.viewportWidth);
+		var boundHeight = Math.max(this.imgHeight * this.scale, this.viewportHeight);
+
+		this.figEl.setStyle({
+			width: boundWidth + 'px'
+			,height: boundHeight + 'px'
+		});
+		
+		// update scroller to new content size
+		this.scroller.updateBoundary();
+
+		// apply scroll
+		this.scroller.setOffset({
+			x: this.scrollX || 0
+			,y: this.scrollY || 0
+		});
+	}
+
+});
+
+Ext.reg('imageviewer', Jarvus.mobile.ImageViewer);
--- PinchCarousel.js
+++ PinchCarousel.js
@@ -2,11 +2,12 @@ Ext.ns('Jarvus.mobile');
 
 Jarvus.mobile.PinchCarousel = Ext.extend(Ext.Carousel, {
 
-	imageField: 'image_url'
-	//,captionField: 'caption' // NOT YET IMPLEMENTED
-	,store: false
+	loadingMask: true
 	,images: false
-	,doubleTapScale: 1
+	,store: false
+	,imageField: 'image_url'
+	,previewField: 'image_thumbnail_url'
+	//,captionField: 'caption' // NOT YET IMPLEMENTED
 
 	,initComponent: function() {
 	
@@ -15,11 +16,16 @@ Jarvus.mobile.PinchCarousel = Ext.extend(Ext.Carousel, {
 		if(this.store)
 		{
 			// initialize items from a store if provided
-			this.store.each(this.addImage, this);
+			this.store.each(function(imageRecord) {
+				this.addImage({
+					imageSrc: this.imageField ? imageRecord.get(this.imageField) : false
+					,previewSrc: this.previewField ? imageRecord.get(this.previewField) : false
+				});
+			}, this);
 		}
 		else if(this.images)
 		{
-			// initialize items from an array of URLs
+			// initialize items from an array of URLs or configs
 			Ext.each(this.images, this.addImage, this);
 		}
 		
@@ -28,273 +34,58 @@ Jarvus.mobile.PinchCarousel = Ext.extend(Ext.Carousel, {
 		
 	}
 
-	,afterRender: function() {
-		Jarvus.mobile.PinchCarousel.superclass.afterRender.call(this);
-	
-		if(this.doubleTapScale)
-		{
-			this.mon(this.body, 'doubletap', this.onDoubleTap, this, {delegate: 'img'});
-		}
-	}
+	,addImage: function(image, doLayout) {
 
-	,addImage: function(image) {
-	
-		image = (typeof image == "string") ? image : image.get(this.imageField);
-	
-		var slideCfg = {
-			xtype: 'component'
-			,cls: 'imageBox'
-			,scroll: 'both'
-			,html: [
-				'<figure>'
-					,Ext.LoadingSpinner
-					,'<img src="'+image+'">'
-				,'</figure>'
-			]
-			,listeners: {
-				scope: this
-				,afterrender: this.onAfterImageBoxRender
-			}
-		};
-	
+		image = Ext.apply({
+			xtype: 'imageviewer'
+			,loadingMask: this.loadingMask
+		}, typeof image == "string" ? {imageSrc: image} : image);
+		
 		if(this.rendered)
 		{
-			this.add(slideCfg);
-			this.doLayout();
+			this.add(image);
+			
+			if(doLayout !== false)
+				this.doLayout();
 		}
 		else
 		{
-			this.items.push(slideCfg);
+			this.items.push(image);
 		}
 	}	
-
-	
-	,onAfterImageBoxRender: function(imgBox) {
-	
-		// get viewport size
-		this.viewportWidth = this.viewportWidth || this.getWidth();
-		this.viewportHeight = this.viewportHeight || this.getHeight();
-		
-		// retrieve DOM els
-		imgBox.figEl = imgBox.el.down('figure');
-		imgBox.imgEl = imgBox.figEl.down('img');
-		
-		// apply required styles
-		imgBox.figEl.setStyle({
-			overflow: 'hidden'
-			,display: 'block'
-			,margin: 0
-		});
-		
-		imgBox.imgEl.setStyle({
-			'-webkit-user-drag': 'none'
-			,'-webkit-transform-origin': '0 0'
-			,'visibility': 'hidden'
-		});
-
-	
-		this.mon(imgBox.imgEl, 'load', function() {
-		
-			imgBox.imgWidth = imgBox.imgEl.dom.width
-			imgBox.imgHeight = imgBox.imgEl.dom.height;
-			
-			// calculate and apply initial scale to fit image to screen
-			if(imgBox.imgWidth > this.viewportWidth || imgBox.imgHeight > this.viewportHeight)
-				imgBox.scale = imgBox.baseScale = Math.min(this.viewportWidth/imgBox.imgWidth, this.viewportHeight/imgBox.imgHeight);
-			else
-				imgBox.scale = imgBox.baseScale = 1;
-			
-			// set initial translation to center
-			imgBox.translateX = imgBox.translateBaseX = (this.viewportWidth - imgBox.baseScale * imgBox.imgWidth) / 2;
-			imgBox.translateY = imgBox.translateBaseY = (this.viewportHeight - imgBox.baseScale * imgBox.imgHeight) / 2;
-			
-			this.applyTransform(imgBox);
-			
-
-			// remove spinner and show image
-			imgBox.el.down('.x-loading-spinner').remove();
-			imgBox.imgEl.setStyle({ visibility: 'visible' });
-			
-			// initialize scroller configuration
-			this.adjustScroller(imgBox);
-
-			// wire pinch listeners
-			imgBox.imgEl.on({
-				scope: this
-				,pinchstart: function(ev, t) {
-				
-					// disable scrolling during pinch
-					imgBox.scroller.stopMomentumAnimation();
-					imgBox.scroller.disable();
-					
-					// store beginning scale
-					imgBox.startScale = imgBox.scale;
-					
-					// calculate touch midpoint relative to image viewport
-					imgBox.originViewportX = (ev.touches[0].clientX + ev.touches[1].clientX) / 2 - imgBox.el.getX();
-					imgBox.originViewportY = (ev.touches[0].clientY + ev.touches[1].clientY) / 2 - imgBox.el.getY();
-					
-					// translate viewport origin to position on scaled image
-					imgBox.originScaledImgX = imgBox.originViewportX - imgBox.scroller.offset.x - imgBox.translateX;
-					imgBox.originScaledImgY = imgBox.originViewportY - imgBox.scroller.offset.y - imgBox.translateY;
-					
-					// unscale to find origin on full size image
-					imgBox.originFullImgX = imgBox.originScaledImgX / imgBox.scale;
-					imgBox.originFullImgY = imgBox.originScaledImgY / imgBox.scale;
-					
-					// calculate translation needed to counteract new origin and keep image in same position on screen
-					imgBox.translateX += (-1 * ((imgBox.imgWidth*(1-imgBox.scale)) * (imgBox.originFullImgX/imgBox.imgWidth)));
-					imgBox.translateY += (-1 * ((imgBox.imgHeight*(1-imgBox.scale)) * (imgBox.originFullImgY/imgBox.imgHeight)))
-
-					// apply new origin
-					imgBox.imgEl.dom.style.webkitTransformOrigin = (imgBox.originFullImgX)+'px '+(imgBox.originFullImgY)+'px';
-
-					// apply translate and scale CSS
-					this.applyTransform(imgBox);
-					
-					
-				}
-				,pinch: function(ev, t) {
-					// prevent scaling to smaller than screen size
-					imgBox.scale = Ext.util.Numbers.constrain(ev.scale * imgBox.startScale, imgBox.baseScale);
-					this.applyTransform(imgBox);
-				}
-				,pinchend: function(ev, t) {
-				
-					// calculate rescaled origin
-					imgBox.originReScaledImgX = imgBox.originScaledImgX * (imgBox.scale / imgBox.startScale);
-					imgBox.originReScaledImgY = imgBox.originScaledImgY * (imgBox.scale / imgBox.startScale);
-				
-					// calculate new translation
-					imgBox.translateX = imgBox.originViewportX - imgBox.originReScaledImgX;
-					imgBox.translateY = imgBox.originViewportY - imgBox.originReScaledImgY;
-						
-					// transfer negative translations to scroll offset
-					imgBox.scrollX = imgBox.scrollY = 0;
-					
-					if(imgBox.translateX < 0)
-					{
-						imgBox.scrollX = imgBox.translateX;
-						imgBox.translateX = 0;
-					}
-					if(imgBox.translateY < 0)
-					{
-						imgBox.scrollY = imgBox.translateY;
-						imgBox.translateY = 0;
-					}
-						
-					
-					// reset origin and update transform with new translation
-					imgBox.imgEl.dom.style.webkitTransformOrigin = '0 0';
-					this.applyTransform(imgBox);
-		
-					// adjust scroll container
-					this.adjustScroller(imgBox);
-					
-					// apply scroll
-					imgBox.scroller.setOffset({
-						x: imgBox.scrollX
-						,y: imgBox.scrollY
-					});
-					
-				}
-			});
-		}, this);
-		
-	}
-	
-	
-	,onDoubleTap: function(ev, t) {
-		
-		var imgBox = Ext.getCmp(ev.getTarget('.imageBox').id)
-			,oldScale = imgBox.scale
-			,newScale = oldScale + this.doubleTapScale
-			,originViewportX = ev.pageX - imgBox.el.getX()
-			,originViewportY = ev.pageY - imgBox.el.getY()
-			,originScaledImgX = originViewportX - imgBox.scroller.offset.x - imgBox.translateX
-			,originScaledImgY = originViewportY - imgBox.scroller.offset.y - imgBox.translateY
-			,originReScaledImgX = originScaledImgX * (newScale / oldScale)
-			,originReScaledImgY = originScaledImgY * (newScale / oldScale);
-			
-		// set scale and translation
-		imgBox.scale = newScale;
-		imgBox.translateX = originViewportX - originReScaledImgX;
-		imgBox.translateY = originViewportY - originReScaledImgY;
-			
-		// transfer negative translations to scroll offset
-		imgBox.scrollX = imgBox.scrollY = 0;
-		
-		if(imgBox.translateX < 0)
-		{
-			imgBox.scrollX = imgBox.translateX;
-			imgBox.translateX = 0;
-		}
-		if(imgBox.translateY < 0)
-		{
-			imgBox.scrollY = imgBox.translateY;
-			imgBox.translateY = 0;
-		}
-			
-		
-		// reset origin and update transform with new translation
-		this.applyTransform(imgBox);
-
-		// adjust scroll container
-		this.adjustScroller(imgBox);
-		
-		// apply scroll
-		imgBox.scroller.setOffset({
-			x: imgBox.scrollX
-			,y: imgBox.scrollY
-		});
-	}
-		
-	
-	,applyTransform: function(imgBox) {
-	
-		if(Ext.is.Android)
-			imgBox.imgEl.dom.style.webkitTransform = 'translate('+imgBox.translateX+'px, '+imgBox.translateY+'px) scale('+imgBox.scale+','+imgBox.scale+')';
-		else
-			imgBox.imgEl.dom.style.webkitTransform = 'translate3d('+imgBox.translateX+'px, '+imgBox.translateY+'px, 0) scale3d('+imgBox.scale+','+imgBox.scale+',1)';
-		
-	}
-
-
-	,adjustScroller: function(imgBox) {
-	
-		// disable scrolling if zoomed out completely, else enable it
-		if(imgBox.scale == imgBox.baseScale)
-			imgBox.scroller.disable();
-		else
-			imgBox.scroller.enable();
-		
-		// size container to final image size
-		var boundWidth = Math.max(imgBox.imgWidth * imgBox.scale, this.viewportWidth);
-		var boundHeight = Math.max(imgBox.imgHeight * imgBox.scale, this.viewportHeight);
-
-		imgBox.figEl.setStyle({
-			width: boundWidth + 'px'
-			,height: boundHeight + 'px'
-		});
-		
-		// update scroller to new content size
-		imgBox.scroller.updateBoundary();
-	}
 
 	// suppress carousel drag on multi-touch
 	,onDragStart: function(e) {
 		if(e.targetTouches.length == 1)
 			Ext.Carousel.prototype.onDragStart.call(this, e);
+/*
+		if(e.targetTouches.length == 1 && (!this.getActiveItem().scroller || this.getActiveItem().scroller.disabled))
+			Ext.Carousel.prototype.onDragStart.call(this, e);
+		else
+			console.log('suppressing drag start');
+*/
 	}
 	,onDrag: function(e) {
 		if(e.targetTouches.length == 1)
 			Ext.Carousel.prototype.onDrag.call(this, e);
+/*
+		if(e.targetTouches.length == 1 && (!this.getActiveItem().scroller || this.getActiveItem().scroller.disabled))
+			Ext.Carousel.prototype.onDrag.call(this, e);
+		else
+			console.log('suppressing drag');
+*/
 	}
 	,onDragEnd: function(e) {
 		if(e.targetTouches.length < 2)
 			Ext.Carousel.prototype.onDragEnd.call(this, e);
+/*
+		if(e.targetTouches.length < 2 && (!this.getActiveItem().scroller || this.getActiveItem().scroller.disabled))
+			Ext.Carousel.prototype.onDragEnd.call(this, e);
+		else
+			console.log('suppressing drag end');
+*/
 	}
 
 });
 
-Ext.reg('pinch-carousel', Jarvus.mobile.PinchCarousel);
+Ext.reg('pinchcarousel', Jarvus.mobile.PinchCarousel);
